7fea16519c3e653edb286eedee2aaf0772e71511
hujinping <870469237@qq.com>
2018-10-12 18:39:18 +0800

    添加战斗场景

---
+++ assets/Script/View/fight/Air.ts
@@ -0,0 +1,18 @@
+
+
+const {ccclass, property} = cc._decorator;
+@ccclass
+export default class NewClass extends cc.Component {
+
+    @property(Number)
+    bulletHurt:Number=0;
+
+    @property(Number)
+    lifeValue:Number=0;
+
+
+    
+
+    
+
+}
