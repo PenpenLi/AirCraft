9b8097cc8b9f60792a83cf721b65627501319427
hujinping <546830227@qq.com>
2018-10-17 11:38:41 +0800

    战斗场景优化

--- assets/Script/View/fight/Air.ts
+++ assets/Script/View/fight/Air.ts
@@ -1,21 +1,37 @@
 import GameCtr from "../../Controller/GameCtr";
 import NodePoolManager from "../../Common/NodePoolManager";
-
+enum Attack{
+    UP=1,
+    DOWN=-1
+}
 
 const {ccclass, property} = cc._decorator;
 @ccclass
 export default class NewClass extends cc.Component {
     _lifeValue=null;
     _currentLifeValue=null;
+    _level=null;
     _bulletHurt=null;
+    _deadEft=null;
     _bulletSpeed=3;
     _attackInterval=2;
     _bulletsArr=[];
     _skin=null;
     _lifeBar=null;
     _isEnemy=false;
+    _isBoss=false;
     _bubbleHurtPool=null;
+    _attackDirection=null;
+    _bulletCount=0;
 
+    @property(cc.SpriteFrame)
+    enemySkins:cc.SpriteFrame[]=[];
+
+    @property(cc.SpriteFrame)
+    bossSkins:cc.SpriteFrame[]=[];
+
+    @property(cc.SpriteFrame)
+    selfSkins:cc.SpriteFrame[]=[];
    
     @property(cc.Prefab)
     bullet:cc.Prefab=null;
@@ -23,44 +39,54 @@ export default class NewClass extends cc.Component {
     @property(cc.Prefab)
     bubbleHurt:cc.Prefab=null;
 
+    @property(cc.Prefab)
+    deadEft:cc.Prefab=null;
+
     onLoad(){
-        this._skin=this.node.getChildByName("skin");
+        this._skin=this.node.getComponent(cc.Sprite);
         this._lifeBar=this.node.getChildByName("lifeBar");
         this._lifeBar.active=false;
-
         this._bubbleHurtPool=new cc.NodePool();
     }
 
     startAttack(){
+        this._attackInterval=this._isBoss?0.5:3
         this.schedule(this.doAttacked, this._attackInterval);
     }
-
 
     init(data){
         this._lifeValue=data.lifeValue;
         this._bulletHurt=data.bulletHurt;
         this._currentLifeValue=data.lifeValue;
         this._isEnemy=data.isEnemy;
+        this._isBoss=data.isBoss;
+        this._level=data.level;
+        this._attackDirection=this._isEnemy?Attack.DOWN:Attack.UP;
+        this._bulletCount=this._isBoss?10:3;
+
         this.initBullets();
         this.initBubbleHurt();
+        this.initDeadEft();
+        this.setSkin();
         if(this._isEnemy){
-            this._skin.scaleY=-1;
             this._lifeBar.active=true;
             this._lifeBar.getComponent(cc.ProgressBar).progress=1;
         }
+
+        if(this._isBoss){
+            this._lifeBar.y+=100;
+        }
     }
 
-    
-
     initBullets(){
-        for(let i=0;i<3;i++){
+        for(let i=0;i<this._bulletCount;i++){
             let bullet =cc.instantiate(this.bullet);
             bullet.parent=cc.find("Canvas");
             bullet.active=false;
             bullet.x=this.node.x;
             bullet.y=this.node.y+150;
 
-            bullet.getComponent("Bullet").init({hurt:this._bulletHurt,isEnemy:this._isEnemy})
+            bullet.getComponent("Bullet").init({hurt:this._bulletHurt,isEnemy:this._isEnemy,level:this._level,isBoss:this._isBoss})
             this._bulletsArr.push(bullet);
         }
     }
@@ -72,11 +98,29 @@ export default class NewClass extends cc.Component {
         }
     }
 
+    initDeadEft(){
+        this._deadEft=cc.instantiate(this.deadEft);
+        this._deadEft.parent=cc.find("Canvas");
+        this._deadEft.active=false;
+    }
+
+    setSkin(){
+        if(this._isBoss){
+            this._skin.spriteFrame=this.bossSkins[this._level-1];
+            return;
+        }
+        if(this._isEnemy){
+            this._skin.spriteFrame=this.enemySkins[this._level-1];
+        }else{
+            this._skin.spriteFrame=this.selfSkins[this._level-1];
+        }
+    }
+
 
     getFreeBullet(){
         for(let i =0;i<this._bulletsArr.length;i++){
             if(!this._bulletsArr[i].active){
-                return this._bulletsArr[i]
+                return this._bulletsArr[i];
             }
         }
         return null;
@@ -85,14 +129,16 @@ export default class NewClass extends cc.Component {
     //发送子弹 攻击对方
     doAttacked(){
         let bullet=this.getFreeBullet();
+        let targetPosX=this._isBoss?Math.random()*1400-700:0
+
         if(bullet){
             bullet.stopAllActions();
             bullet.active=true;
             bullet.tag=10086;
             bullet.x=this.node.x;
-            bullet.y=this.node.y+150*this._skin.scaleY;
+            bullet.y=this.node.y+150*this._attackDirection;
             bullet.runAction(cc.sequence(
-                cc.moveBy(this._bulletSpeed,cc.p(0,1500*this._skin.scaleY)),
+                cc.moveBy(this._bulletSpeed,cc.p(targetPosX,1500*this._attackDirection)),
                 cc.callFunc(()=>{
                     bullet.active=false;
                 })
@@ -108,8 +154,11 @@ export default class NewClass extends cc.Component {
             this.showHurt(hurt);
         }
         if(this._currentLifeValue<=0){
+            if(this._isBoss){
+                GameCtr.getInstance().getFight().doUpLevel();
+            }
             GameCtr.getInstance().getFight().removeAir(this.node);
-            this.node.destroy();
+            this.showDeadEft();
         }
     }
 
@@ -135,6 +184,14 @@ export default class NewClass extends cc.Component {
                 this._bubbleHurtPool.put(bubbleHurt)
             })
         ))
+    }
+
+    showDeadEft(){
+        this._deadEft.active=true;
+        this._deadEft.x=this.node.x;
+        this._deadEft.y=this.node.y;
+        this._deadEft.getComponent(cc.Animation).play();
+        this.node.destroy();
     }
 
     //敌人随机移动
--- assets/Script/View/fight/Bullet.ts
+++ assets/Script/View/fight/Bullet.ts
@@ -2,12 +2,42 @@
 const {ccclass, property} = cc._decorator;
 @ccclass
 export default class NewClass extends cc.Component {
-    _hurt:null;
-    _isEnemy:null;
+    _skin=null;
+    _hurt=null;
+    _isEnemy=null;
+    _isBoss=null;
+    _level=null;
 
+
+    @property(cc.SpriteFrame)
+    enemyBulletSkins:cc.SpriteFrame[]=[];
+    
+    @property(cc.SpriteFrame)
+    selfBulletSkins:cc.SpriteFrame[]=[];
+
+    @property(cc.SpriteFrame)
+    bossBulletSkins:cc.SpriteFrame[]=[];
+
+   
     init(data){
         this._hurt=data.hurt;
         this._isEnemy=data.isEnemy;
+        this._level=data.level;
+        this._isBoss=data.isBoss;
+        this.setSkin();
+    }
+
+    setSkin(){
+        this._skin=this.node.getComponent(cc.Sprite);
+        if(this._isBoss){
+            this._skin.spriteFrame=this.bossBulletSkins[this._level-1];
+            return;
+        }
+        if(this._isEnemy){
+            this._skin.spriteFrame=this.enemyBulletSkins[this._level-1];
+        }else{
+            this._skin.spriteFrame=this.selfBulletSkins[this._level-1];
+        }
     }
 
     getIsEmeny(){
